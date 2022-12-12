b34e029034ab608159e60ea4bcb0fec5e739801a
Mathieu Scolas <worming@hotmail.be>
2022-12-12 10:22:58 +0100

    add few dotnet things

--- cmd/generate/config/rules/dotnet.go
+++ cmd/generate/config/rules/dotnet.go
@@ -1,18 +1,30 @@
 package rules
 
-import "github.com/zricethezav/gitleaks/v8/config"
+import (
+	"github.com/zricethezav/gitleaks/v8/config"
+	"regexp"
+)
 
-func DotnetAppSettings() *config.Rule {
+func DotnetAppSettingsPasswordValue() *config.Rule {
 	r := config.Rule{
 		Description: "Dotnet Appsettings ConnectionStrings",
 		RuleID:      "dotnet-appsettings-connection-strings",
-		Regex:       generateUniqueTokenRegex("(?i)((key|api|token|secret|password)[a-z0-9_ .\\-,]{0,25})['\\\"]?(=|>|:=|\\|\\|:|<=|=>|:).{0,5}['\\\"]([0-9a-zA-Z!\"#$%&'()*+,-.\\/:;<=>?@[\\\\\\]^_`{|}~]{8,64})['\\\"]\n\n,"),
-		Path:        generateUniqueTokenRegex("**/AppSettings*.json"),
+		Regex:       regexp.MustCompile(`[pP]assword=[^;":]`),
+		Keywords: []string{"ConnectionStrings"},
 	}
 
-	tps := []string{`"ConnectionStrings": {
-    		"BloggingDatabase": "Server=(localdb)\\mssqllocaldb;Database=EFGetStarted.ConsoleApp.NewDb;Trusted_Connection=True;"
-  		},
-`}
+	tps := []string{`"ConnectionStrings = { BloggingDatabase": "Server=(localdb)\\mssqllocaldb;Database=EFGetStarted.ConsoleApp.NewDb;User=user;Password=pswd" }`}
+	return validate(r, tps, nil)
+}
+
+
+func DotnetAppSettingsEsbPassword() *config.Rule {
+	r := config.Rule{
+		Description: "Dotnet Esb Password",
+		RuleID:      "dotnet-esb-password",
+		Regex:       regexp.MustCompile(`[pP]assword":\s*".+"`),
+	}
+
+	tps := []string{`"Password": "vqlu3"`}
 	return validate(r, tps, nil)
 }
